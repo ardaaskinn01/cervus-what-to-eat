7df1796d621b59cb042a80d9d24d5119a2d87f3e
ardaaskinn01 <ardaaskindm@gmail.com>
2026-08-04 17:54:03 +0300

    new updates

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -13,7 +13,7 @@ keyProperties.load(keyPropertiesFile.inputStream())
 
 android {
     namespace = "com.cervus.whattoeat"
-    compileSdk = 35
+    compileSdk = 36
     ndkVersion = "27.0.12077973"
 
     compileOptions {
@@ -38,7 +38,7 @@ android {
     defaultConfig {
         applicationId = "com.cervus.whattoeat"
         minSdk = 21
-        targetSdk = 35
+        targetSdk = 36
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
